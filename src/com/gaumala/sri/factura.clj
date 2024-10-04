648c090857c4d1e3c06bf39b1da192ccc2b45a6c
Gabriel Aumala <gaumala@protonmail.com>
2024-10-04 13:44:54 -0500

    improve gen-factura param structure

--- src/com/gaumala/sri/factura.clj
+++ src/com/gaumala/sri/factura.clj
@@ -68,13 +68,25 @@
    :attrs {:nombre nombre}
    :content [(str texto)]})
 
-(defn gen-factura [params]
-  (let [{:keys [detalles pagos totalConImpuestos infoAdicional]} params
-        tipoEmision "1" ;offline solo tiene tipo 1
-        claveAcceso (gen-clave-acceso params)
-        factura-data (conj params {:tipoEmision tipoEmision
-                                   :claveAcceso claveAcceso})
-        simple-tag (simple-tag-hof factura-data)
+(defn gen-info-tributaria [params]
+  (let [simple-tag (simple-tag-hof params)]
+    {:tag :infoTributaria
+     :attrs {}
+     :content (filter some? [(simple-tag :ambiente)
+                             (simple-tag :tipoEmision)
+                             (simple-tag :razonSocial)
+                             (simple-tag :nombreComercial)
+                             (simple-tag :ruc)
+                             (simple-tag :claveAcceso)
+                             (simple-tag :codDoc)
+                             (simple-tag :estab)
+                             (simple-tag :ptoEmi)
+                             (simple-tag :secuencial)
+                             (simple-tag :dirMatriz)])}))
+
+(defn gen-info-factura [params]
+  (let [simple-tag (simple-tag-hof params)
+        {:keys [pagos totalConImpuestos]} params
         total-con-impuestos-tag {:tag :totalConImpuestos
                                  :attrs {}
                                  :content (map gen-total-impuesto
@@ -82,46 +94,54 @@
         pagos-tag {:tag :pagos
                    :attrs {}
                    :content (map gen-pago pagos)}]
+    {:tag :infoFactura
+     :attrs {}
+     :content (filter some? [(simple-tag :fechaEmision)
+                             (simple-tag :dirEstablecimiento)
+                             (simple-tag :contribuyenteEspecial)
+                             (simple-tag :obligadoContabilidad)
+                             (simple-tag :tipoIdentificacionComprador)
+                             (simple-tag :guiaRemision)
+                             (simple-tag :razonSocialComprador)
+                             (simple-tag :identificacionComprador)
+                             (simple-tag :direccionComprador)
+                             (simple-tag :totalSinImpuestos)
+                             (simple-tag :totalDescuento)
+                             total-con-impuestos-tag
+                             (simple-tag :propina)
+                             (simple-tag :importeTotal)
+                             (simple-tag :moneda)
+                             pagos-tag
+                             (simple-tag :valorRetIva)
+                             (simple-tag :valorRetRenta)])}))
+
+(defn- complete-info-tributaria [params fechaEmision codigo]
+  (let [tipoEmision (or (:tipoEmision params) "1") ;offline solo tiene tipo 1
+        claveAcceso (or (:claveAcceso params)
+                        (gen-clave-acceso (conj params
+                                                {:codigoNumerico codigo
+                                                 :fechaEmision fechaEmision})))]
+    (conj params
+          {:tipoEmision tipoEmision
+           :claveAcceso claveAcceso})))
+
+(defn gen-factura [params codigo]
+  (let [{:keys [infoFactura detalles infoAdicional]} params
+        infoTributaria (complete-info-tributaria (:infoTributaria params)
+                                                 (:fechaEmision infoFactura)
+                                                 codigo)
+        info-tributaria-tag (gen-info-tributaria infoTributaria)
+        info-factura-tag (gen-info-factura infoFactura)
+        detalles-tag {:tag :detalles
+                      :attrs {}
+                      :content (map gen-detalle detalles)}
+        info-adicional-tag {:tag :infoAdicional
+                            :attrs {}
+                            :content (map gen-campo-adicional
+                                          infoAdicional)}]
     {:tag :factura
      :attrs {:id "comprobante" :version "1.0.0"}
-     :content [{:tag :infoTributaria
-                :attrs {}
-                :content (filter some? [(simple-tag :ambiente)
-                                        (simple-tag :tipoEmision)
-                                        (simple-tag :razonSocial)
-                                        (simple-tag :nombreComercial)
-                                        (simple-tag :ruc)
-                                        (simple-tag :claveAcceso)
-                                        (simple-tag :codDoc)
-                                        (simple-tag :estab)
-                                        (simple-tag :ptoEmi)
-                                        (simple-tag :secuencial)
-                                        (simple-tag :dirMatriz)])}
-               {:tag :infoFactura
-                :attrs {}
-                :content (filter some?
-                                 [(simple-tag :fechaEmision)
-                                  (simple-tag :dirEstablecimiento)
-                                  (simple-tag :contribuyenteEspecial)
-                                  (simple-tag :obligadoContabilidad)
-                                  (simple-tag :tipoIdentificacionComprador)
-                                  (simple-tag :guiaRemision)
-                                  (simple-tag :razonSocialComprador)
-                                  (simple-tag :identificacionComprador)
-                                  (simple-tag :direccionComprador)
-                                  (simple-tag :totalSinImpuestos)
-                                  (simple-tag :totalDescuento)
-                                  total-con-impuestos-tag
-                                  (simple-tag :propina)
-                                  (simple-tag :importeTotal)
-                                  (simple-tag :moneda)
-                                  pagos-tag
-                                  (simple-tag :valorRetIva)
-                                  (simple-tag :valorRetRenta)])}
-               {:tag :detalles
-                :attrs {}
-                :content (map gen-detalle detalles)}
-               {:tag :infoAdicional
-                :attrs {}
-                :content (map gen-campo-adicional
-                              infoAdicional)}]}))
+     :content [info-tributaria-tag
+               info-factura-tag
+               detalles-tag
+               info-adicional-tag]}))
